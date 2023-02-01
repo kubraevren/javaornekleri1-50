95cb5a20a189b2531d06c31c43d868a635c3d6b3
Hatice Kübra Evren <19010310050@ogrenci.bartin.edu.tr>
2023-02-01 23:37:43 +0300

    1. soru 1-02-2023

---
+++ soru1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class soru1 {
+// klavyeden tek sayi al ve desen olustur: ornek: 5 
+//        *	
+//        *
+//	  * * * * *  
+//        * 
+//        * 
+	public static void main(String[] args) {
+		Scanner girdi = new Scanner(System.in);
+		System.out.print("tek sayi giriniz: ");
+		int sayi = girdi.nextInt();
+		if (sayi % 2 == 0) {
+			System.out.println("lutfen tek sayi giriniz. ");
+		} else {
+			YildizOlustur(sayi);
+			 
+			 for(int t=1;t<=sayi;t++) {
+				 System.out.print("*");
+			 }
+			 System.out.println();
+			 YildizOlustur(sayi);
+			 
+			 
+			 
+		}
+	}
+		
+		public static void YildizOlustur(int sayi){
+			 for(int k=1;k<=(sayi/2);k++) {
+		           for(int i=1;i<=(sayi/2);i++) {
+		        	   System.out.print(" ");
+		           }
+		           System.out.print("*");
+		           System.out.println();
+					 }
+		}
+
+	
+}
